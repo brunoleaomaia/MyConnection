978ae904c9d3a5ae201ccbf1dcfe46c5f0c229ea
Bruno Maia <brunoleaomaia@gmail.com>
2012-08-08 18:05:55 -0300

    fbconfig added

---
+++ fbconfig.php
@@ -0,0 +1,11 @@
+<?php
+	//FACEBOOK PHP SDK
+	require 'inc/facebook/facebook.php';
+	
+	//APP CONFIG
+	$apiId              = '';
+	$apiSecret          = '';
+
+	//URL CONFIG
+	$appUrl				= 'http://owd.jqmphp.com';
+?>
